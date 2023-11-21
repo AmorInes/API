b663f58e651115392f6a2dc1b11ab5ff71dcbe5d
jcricklin <jcricklin@gmail.com>
2023-11-21 11:36:44 +0100

    parallelism management

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+import logging
 # test afin  de pouvoir répliquer arima
 from AutoArima import TrainAutoArima, PredictAutoArima, GetFeaturesInterpretation
 # import utilsTCN
+from waitress import serve
+from concurrent.futures import ThreadPoolExecutor
 import data_prep
 import pandas as pd
 import numpy as np
@@ -13,86 +16,22 @@ NB_PRIX = 5
 
 #On a desoin de plusieurs version de prepare data
 
+def process_product_ARIMA(x_future, feature_quantite_final ,final_df,target,nb_jours,exogenous) :
 
-
-
-
-@app.route('/api/modelbooper/prixpermanent', methods=['POST'])
-def receive_data():
-    Product_features_json = request.json['LIST_HISTO']
-    Product_quantity_json = request.json['LIST_QUANTITE']
-    Product_future_features_json = request.json['LIST_FUTURE']
-    
-    # print(Product_features_json)
-    # print(Product_quantity_json)
-    # print(Product_future_features_json)
-    # Create a list to store DataFrames
-    
     #Creat a dictionary which contains all information need
     preds = {}
-    ##### 
-    #Il faut que face une fonction pour retraiter les données : 
-    #####
-    
-    dfs = []
-
-    # Loop through both lists simultaneously
-    for features, quantity in zip(Product_features_json, Product_quantity_json):
-        # Convert the features dictionary to DataFrame
-        df = pd.DataFrame([features])
-        # Add the quantity to the DataFrame
-        df['QUANTITE'] = quantity
-        dfs.append(df)
-
-
-    # Concatenate all DataFrames together
-    final_df = pd.concat(dfs, ignore_index=True)
-    
-    # Final_df['DATE_IMPORT'] = pd.to_datetime(final_df['DATE_IMPORT'])
-    final_df = final_df.set_index('DATE_IMPORT')
-    
-    # We then have to see if our date are continuous : 
-    
-    
-    # Convert the received JSON data to a DataFrame
-    target = 'QUANTITE'
-    exogenous = [x for x in request.json['LIST_PARAMETRE'] if x != target]
-    
-    
-    #Convert to Numeric: 
-    final_df[exogenous] = final_df[exogenous].apply(pd.to_numeric, errors='coerce')
-    final_df[target] = pd.to_numeric(final_df[target], errors='coerce')
-    
-    #Handle NaNs: 
-    final_df.dropna(inplace=True)
-
-    #fill them with a specific value, like zero: 
-    final_df.fillna(0, inplace=True)
-    
-    #Inspect Problematic Columns:       
-    jour_max_connu = int(len(final_df)*0.70)
-    
-    
-    X_train = final_df[:jour_max_connu]
-    X_test = final_df[jour_max_connu:]
-    x_future = pd.DataFrame(Product_future_features_json) 
-    
-    # The choice of prediction set 
-    nb_jours = len(x_future)
-    target = 'QUANTITE'
-    
-    # x_future['DATE_IMPORT'] = pd.to_datetime(x_future['DATE_IMPORT'])
-    x_future =  x_future.set_index('DATE_TMP')
 
     # forecasting with the last price :
     model = TrainAutoArima(final_df, exogenous, target)
-    preds['Prix_0'] = PredictAutoArima(model, x_future, exogenous)
+    preds['QUANTITE_AJUSTE'] = PredictAutoArima(model, feature_quantite_final, exogenous)
+    preds['QUANTITE_0'] = PredictAutoArima(model, x_future, exogenous)
     
-    print(preds['Prix_0'])
+    # print(preds['QUANTITE_0'])
     # some variation test :
     prix_min = min(final_df['PARAM_PRIX'])
     prix_max = max(final_df['PARAM_PRIX'])
     last_price = final_df['PARAM_PRIX'][-1]
+
     vec_prix_test = [prix_min + i*(prix_max - prix_min) / (NB_PRIX - 1 ) for i in range(NB_PRIX)]
     vec_promo_test = np.arange(0, 0.95, 0.05).tolist()
     
@@ -107,23 +46,20 @@ def receive_data():
     x_future['PARAM_PRIX'] = [last_price] * nb_jours 
     # Change prediction values with the promotion value :  
     
-    
     for promo in vec_promo_test : 
 
         x_future['PARAM_PROMO'] = [promo] * nb_jours
         preds[f'PROMO_{int(promo*100)}'] = PredictAutoArima(model, x_future, exogenous)
         
     coefficients = GetFeaturesInterpretation(model)
-    if coefficients is not None:
-        print("Model Coefficients:", coefficients)
+    # if coefficients is not None:
+    #     # print("Model Coefficients:", coefficients)
     preds['ELASTICITE'] = coefficients
-        
-
-    
+            
     # df_prediction = pd.DataFrame(preds)
     # print(df_prediction)
     # Check if the series is not empty (it change)
- 
+
     # json_string = df_prediction.to_json()
     # #print(json_string)  # This will print the JSON representation of your series
     # preds_converted = {key: value.dt.strftime('%Y-%m-%d').tolist() if hasattr(value, 'dt') else value.tolist() for key, value in preds.items()}
@@ -143,16 +79,118 @@ def receive_data():
 
     # Traiter spécifiquement l'élasticité si c'est un dictionnaire ou une série pandas
     if isinstance(preds['ELASTICITE'], (dict, pd.Series)):
-        preds_converted['ELASTICITE'] = preds['ELASTICITE'].to_dict() if isinstance(preds['ELASTICITE'], pd.Series) else preds['ELASTICITE']
+        preds_converted['ELASTICITE'] = preds['ELASTICITE'].to_dict()  if isinstance(preds['ELASTICITE'], pd.Series)  else preds['ELASTICITE']
+
+    preds_converted['PRIX_INTERVAL'] = vec_prix_test
 
     # Convertir le dictionnaire en JSON
     json_output = json.dumps(preds_converted, indent=4)
-    print(json_output)
+    return json_output
+
+def process_data_ARIMA(Product_features_json, Product_quantity_json, Product_future_features_json, Product_Id_produit_json) :
+    ##### 
+    #Il faut que face une fonction pour retraiter les données : 
+    #####
+    dfs = []
+
+    # print(f'len of the sales quantity {len(Product_quantity_json)}')
+    # print(f'len of the feature quantity {len(Product_features_json)}')
+   
+
+    # Loop through both lists simultaneously
+    if Product_features_json and Product_quantity_json and len(Product_features_json) == len(Product_quantity_json):
+        for features, quantity in zip(Product_features_json, Product_quantity_json):
+            try:
+                df = pd.DataFrame([features])
+                df['QUANTITE'] = quantity
+                dfs.append(df)
+            except Exception as e:
+                print(f"Error processing features: {features}, quantity: {quantity}. Error: {e}")
+
+        if not dfs:
+            print("No dataframes were created. Check the input data.")
+    else:
+        print(f"Input data lists are empty or of different lengths.{len(Product_features_json)}{len(Product_quantity_json)}")
+
+    # Concatenate all DataFrames together
+
+    final_df = pd.concat(dfs, ignore_index=True)
+    # print(f'Original dataset columns {final_df.columns}')
 
 
-    print("je suis là !")
-    return json_output ,200
+    # final_df['DATE_IMPORT'] = pd.to_datetime(final_df['DATE_IMPORT'], format='%d/%m/%Y')
+    final_df = final_df.set_index('DATE_IMPORT')
+    
+    # We then have to see if our date are continuous : 
+    # print(f'index of the dataframe : {final_df.index}')
+    
+    # Convert the received JSON data to a DataFrame : 
+    target = 'QUANTITE'
+    exogenous = [x for x in request.json['LIST_PARAMETRE'] if x != target]
+    # print(f'Used Features : {exogenous}')
+    
+    #Convert to Numeric: 
+    final_df[exogenous] = final_df[exogenous].apply(pd.to_numeric, errors='coerce')
+    final_df[target] = pd.to_numeric(final_df[target], errors='coerce')
+    
+    #Handle NaNs: 
+    final_df.dropna(inplace=True)
+    #fill them with a specific value, like zero: 
+    final_df.fillna(0, inplace=True)
+    #Inspect Problematic Columns:       
+    jour_max_connu = int(len(final_df)*0.70)
+    
+    
+    X_train = final_df[:jour_max_connu]
+    X_test = final_df[jour_max_connu:]
+    x_future = pd.DataFrame(Product_future_features_json) 
+    
+    # The choice of prediction set 
+    nb_jours = len(x_future)
+    target = 'QUANTITE'
+
+
+    # print(f'features contained in {x_future.columns}')
+    # x_future['DATE_TMP'] = pd.to_datetime(x_future['DATE_TMP'], format='%d/%m/%Y')
+    x_future =  x_future.set_index('DATE_TMP')
+    # print(json_output)
+    return x_future, final_df, target, nb_jours, exogenous
+
+@app.route('/api/modelbooper/prixpermanent', methods=['POST'])
+def receive_data():
+
+    Product_features_json = request.json['LIST_HISTO']
+    Product_quantity_json = request.json['LIST_QUANTITE']
+    Product_future_features_json = request.json['LIST_FUTURE']
+    Product_Id_produit_json = request.json['ID_PRODUIT']
+    
+    # print(Product_features_json)
+    # print(Product_Id_produit_json)
+    # print(Product_future_features_json)
+
+    print(f'features len {len(Product_features_json)}')
+    print(f'quantity len {len(Product_quantity_json)}')
+
+    if len(Product_features_json) != 0 and len(Product_quantity_json) != 0 : 
+        # Create a list to store DataFrames : 
+        x_future, final_df, target, nb_jours, exogenous = process_data_ARIMA(Product_features_json, Product_quantity_json, Product_future_features_json, Product_Id_produit_json)
+        
+        # results = process_product_ARIMA(x_future, pd.DataFrame(Product_features_json), final_df, target, nb_jours, exogenous)
+
+        # Create a Pool of procedure : (for compiutation)
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [executor.submit(process_product_ARIMA, x_future, pd.DataFrame(Product_features_json), final_df, target, nb_jours, exogenous) for _ in range(len(Product_Id_produit_json))]
+            results = [future.result() for future in futures]
+        
+        # results = list(executor.map(lambda x: process_product_ARIMA(x_future, pd.DataFrame(Product_features_json), final_df, target, nb_jours, exogenous), [Product_Id_produit_json]))
+        print(jsonify(results))
+        print(f"je suis là ! Avec le produit {Product_Id_produit_json}")
+        return results, 200
+    # else : 
 
 
 if __name__ == '__main__':
-    app.run(debug=True ,host='0.0.0.0', port=5000)
+    # In the app section directly
+    # with ThreadPoolExecutor(max_workers=50) as executor:
+    #     serve(app, host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
